b37aa65bb9476ad5d2721cbcebc3a8e81b5ab7b9
Yan Yermalayeu <yermik2014@yandex.ru>
2024-11-05 01:28:52 +0300

    feat(*): add routing, fix configuration, fix header

--- packages/cart/webpack.config.ts
+++ packages/cart/webpack.config.ts
@@ -1,45 +1,45 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import { type Configuration, container } from "webpack";
-import path from "path";
-import "webpack-dev-server";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { type Configuration, container } from 'webpack';
+import path from 'path';
+import 'webpack-dev-server';
 
 const { ModuleFederationPlugin } = container;
 
 const config: Configuration = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "auto",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 3001,
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "cart",
-      filename: "cart.js",
+      name: 'cart',
+      filename: 'cart.js',
       remotes: {},
       exposes: {
-        "./Cart": "./src/Cart",
+        './Cart': './src/Cart',
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom'],
     }),
     new HTMLWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "public", "index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
 };
--- packages/products/webpack.config.ts
+++ packages/products/webpack.config.ts
@@ -1,45 +1,45 @@
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import { type Configuration, container } from "webpack";
-import path from "path";
-import "webpack-dev-server";
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import { type Configuration, container } from 'webpack';
+import path from 'path';
+import 'webpack-dev-server';
 
 const { ModuleFederationPlugin } = container;
 
 const config: Configuration = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "auto",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 3002,
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "products",
-      filename: "products.js",
+      name: 'products',
+      filename: 'products.js',
       remotes: {},
       exposes: {
-        "./Products": "./src/Products",
+        './Products': './src/Products',
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom'],
     }),
     new HTMLWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "public", "index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
 };
